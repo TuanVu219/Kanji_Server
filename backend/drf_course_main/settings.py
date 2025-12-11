bc15f9ed94b6744d6aa7a4099c75d82be1cb04bd
TuanVu219 <supersaza219@gmail.com>
2025-12-11 17:01:05 +0700

    Initial 9

--- backend/drf_course_main/settings.py
+++ backend/drf_course_main/settings.py
@@ -102,13 +102,9 @@ REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
-
 # -----------------------------
 # Password validation
 # -----------------------------
