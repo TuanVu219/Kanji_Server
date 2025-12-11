fe9e617214b5e8361855e05b9140ef72de4a35db
TuanVu219 <supersaza219@gmail.com>
2025-12-11 17:21:56 +0700

    Initial 10

--- backend/drf_course_main/settings.py
+++ backend/drf_course_main/settings.py
@@ -12,6 +12,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(" ")
 CORS_ALLOW_ALL_ORIGINS = True
+REDIS_URL = os.environ.get("REDIS_URL")
 
 # -----------------------------
 # CORS CONFIG
@@ -102,7 +103,10 @@ REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
     },
 }
 # -----------------------------
